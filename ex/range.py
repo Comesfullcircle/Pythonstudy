b6d97f1dc35b6c35b0cef6ca01e5e390e0913fdf
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2023-07-16 14:27:23 +0900

    Create range.py
    range

---
+++ ex/range.py
@@ -0,0 +1,15 @@
+a = range(5)
+a
+list(range(5))
+list(range(0,5))
+list(range(5,10))
+list(range(0,10,2))
+list(range(0,10,3))
+
+a = range(0, 10 + 1)
+list(a)
+
+b = range(0, int(n/2))
+list(b)
+c = range(0, n//2)
+list(c)

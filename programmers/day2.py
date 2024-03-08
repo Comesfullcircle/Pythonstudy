5f6133ac35b6e6af9a97b596b09ba7b94b9c623f
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-03-08 18:27:53 +0900

    문자열 돌리기, 문자열 뒤집기, 배열 원소의 길이

--- programmers/day2.py
+++ programmers/day2.py
@@ -1,13 +1,22 @@
-//문자열 돌리기
-//문자열 str이 주어집니다.
-//문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
+# 문자열 돌리기
+# 문자열 str이 주어집니다.
+# 문자열을 시계방향으로 90도 돌려서 아래 입출력 예와 같이 출력하는 코드를 작성해 보세요.
 
 for a in input():
     print(a)
 
 
-// 문자열 뒤집기
-//문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요
+
+# 문자열 뒤집기
+# 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요
 def solution(my_string):
     answer = my_string[::-1]
+    return answer
+
+# 배열 원소의 길이
+# 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+def solution(strlist):
+    answer = []
+    for i in strlist:
+        answer.append(len(i))
     return answer

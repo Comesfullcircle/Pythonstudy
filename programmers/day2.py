d86c5894f68523f63a6de57ad31cabb256af1dfe
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-03-12 19:13:28 +0900

    문자열 돌리기, 문자열 뒤집기, 배열 원소의 길이, 배열 자르기, 최댓값 만들기, 뒤에서 5등까지

--- programmers/day2.py
+++ programmers/day2.py
@@ -36,3 +36,10 @@ def solution(numbers):
     numbers.sort(reverse=True)
     answer = numbers[0] * numbers[1]
     return answer
+
+//뒤에서 5등까지
+//정수로 이루어진 리스트 num_list가 주어집니다. num_list에서 가장 작은 5개의 수를 오름차순으로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+def solution(num_list):
+    num_list.sort()
+    return num_list[0:5]

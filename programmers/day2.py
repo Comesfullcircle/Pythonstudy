296616ce0b4978ce0a15cca04eb6b9455d06f065
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-03-12 19:08:27 +0900

    문자열 돌리기, 문자열 뒤집기, 배열 원소의 길이, 배열 자르기, 최댓값 만들기

--- programmers/day2.py
+++ programmers/day2.py
@@ -28,3 +28,11 @@ def solution(strlist):
 def solution(numbers, num1, num2):
     answer = numbers[num1:num2+1]
     return answer
+
+//최댓값 만들기
+//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+def solution(numbers):
+    numbers.sort(reverse=True)
+    answer = numbers[0] * numbers[1]
+    return answer

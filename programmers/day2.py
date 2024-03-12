220eefaf247a423fc1e1bd00f2fa445a2b035434
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-03-12 19:17:59 +0900

    문자열 돌리기, 문자열 뒤집기, 배열 원소의 길이, 배열 자르기, 최댓값 만들기, 뒤에서 5등까지, 문자열로 변환

--- programmers/day2.py
+++ programmers/day2.py
@@ -43,3 +43,10 @@ def solution(numbers):
 def solution(num_list):
     num_list.sort()
     return num_list[0:5]
+
+//문자열로 변환
+//정수 n이 주어질 때, n을 문자열로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+def solution(n):
+    answer = str(n)
+    return answer

fe5487e132388193122345421d276136976e6e4d
윤설이 <86522955+Comesfullcircle@users.noreply.github.com>
2024-03-14 19:15:51 +0900

    문자열 돌리기, 문자열 뒤집기, 배열 원소의 길이, 배열 자르기, 최댓값 만들기, 뒤에서 5등까지, 문자열로 변환, 뒤에서 5등 위로, 짝수는 싫어요, 제곱수 판별하기, 배열 두배, 옷가게 할인받기, 문자열을 정수로 변환하기

--- programmers/day2.py
+++ programmers/day2.py
@@ -100,3 +100,9 @@ def solution(price):
     elif price>=100000:
         price = price * 0.95
     return int(price)
+
+//문자열을 정수로 변환하기
+//숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+def solution(n_str):
+    answer = int(n_str)
+    return answer
